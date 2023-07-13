34373b0e7590db63f882a0447854cb3523354da0
Rithika L <rithika.l@tvsnext.com>
2023-07-13 12:48:02 +0530

    [Rithika] Add : Implemented

--- SnakeLadder/Operation.cs
+++ SnakeLadder/Operation.cs
@@ -10,5 +10,15 @@ namespace SnakeLadder
     {
         const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100, STARTING_POSITION = 0;
         int position = 0, count = 0;
+
+        Random random = new Random();
+        public int DieRoll()
+        {
+            int diePosition = random.Next(1, 7);
+            Console.WriteLine("Player Position" + " " + this.position);
+            count++;
+            return diePosition;
+        }
     }
+
 }
